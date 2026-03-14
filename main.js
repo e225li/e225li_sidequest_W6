0e23e43b5c22bb2f1763ca759552613a7b1fc8c3
e225li <e225li@uwaterloo.ca>
2026-03-13 22:26:32 -0400

    Temporarily comment out audio loading to test if that's causing the blank screen

--- main.js
+++ main.js
@@ -134,19 +134,19 @@ async function boot() {
   soundManager = new SoundManager();
 
   // jump sound effect from Pixabay [5]
-  soundManager.load("jump", "assets/sfx/jump.mp3");
+  // soundManager.load("jump", "assets/sfx/jump.mp3");
 
   // fire extinguisher collect sound effect from Pixabay [4]
-  soundManager.load("collect", "assets/sfx/gemCollect.mp3");
+  // soundManager.load("collect", "assets/sfx/gemCollect.mp3");
 
   // enemy hit sound effect from Pixabay [3]
-  soundManager.load("hitEnemy", "assets/sfx/hitEnemy.mp3");
+  // soundManager.load("hitEnemy", "assets/sfx/hitEnemy.mp3");
 
   // player damage sound effect from Pixabay [2]
-  soundManager.load("damage", "assets/sfx/receiveDamage.mp3");
+  // soundManager.load("damage", "assets/sfx/receiveDamage.mp3");
 
   // background music "Song Of The Forge" by Scott Buckley [1]
-  soundManager.load("music", "assets/sfx/music.mp3");
+  // soundManager.load("music", "assets/sfx/music.mp3");
 
   // --- Parallax layer defs (VIEW) ---
   const defs = levelPkg.level?.view?.parallax ?? [];
